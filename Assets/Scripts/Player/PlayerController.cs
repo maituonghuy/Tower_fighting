87f7ec9d09fe418cf35453baa4fe5e55a676e43c
hatxihoi54 <khuenthe180887@fpt.edu.vn>
2025-07-21 00:06:33 +0700

    comment SetPlayerType

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -631,10 +631,11 @@ public class PlayerController : MonoBehaviour
         damageReductionPercent = 0f;
     }
 
-    public void SetPlayerType(PlayerType type)
-    {
-        playerType = type;
-    }    public void ApplySpeedModifier(string source, float multiplier, float duration = -1f)
+    //public void SetPlayerType(PlayerType type)
+    //{
+    //    playerType = type;
+    //}  
+    public void ApplySpeedModifier(string source, float multiplier, float duration = -1f)
     {
         speedModifiers[source] = multiplier;
         RecalculateSpeed();
